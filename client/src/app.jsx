820758d2c2e1c1fdd900d641988f95165f458bac
Michael Snaddon <msnaddon0991@gmail.com>
2016-10-27 21:42:59 +0100

    added gameview

---
+++ client/src/app.jsx
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+const GameView = require("./components/GameView")
+
+window.onload = () => {
+  ReactDom.render(<GameView/>, document.querySelector("#main-container"));
+}
---
+++ client/src/components/GameView.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+
+class GameView extends react.component{
+  render(){
+    return (
+      <div>
+        <h2>This is GameView</h2>
+      </div>
+      )
+  }
+
+}
