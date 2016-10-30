05c05b16d30d25edd3e8ac83b2f9ef4db54436b3
Michael Snaddon <msnaddon0991@gmail.com>
2016-10-30 08:56:44 +0000

    added dummy data to app and passed game into react component
    Signed-off-by: Michael Snaddon <msnaddon0991@gmail.com>

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -7,5 +7,71 @@ const GameView = require("./components/GameView")
 
 window.onload = () => {
   
-  ReactDom.render(<GameView/>, document.querySelector("#main-container"));
+  let game = new Game(options())
+
+  ReactDom.render(<GameView gameObject={game}/>, document.querySelector("#main-container"));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function options(){
+  let events = [new Event("Started Testing"), new Event("continuing testing")];
+  let inventoryItems = {
+    food: {
+      fruit: new InventoryItem("fruit",2), 
+      meat: new InventoryItem("meat", 4)
+    },
+    weapons: {sword: new InventoryItem("sword", 0)}
+  };
+  
+  let gatherFruit = new Action(function(){
+    this.inventory.food.fruit.quantity += 5;
+    this.eventFeed.push(new Event("you gather fruit"));
+  });
+
+  let hunt = new Action(function(){
+    this.inventory.food.meat.quantity += 3;
+    this.eventFeed.push(new Event("you hunt some meat"));
+  });
+
+  let eatFood = new Action(function(){
+    if (this.inventory.fruit) {
+      this.inventory.food.fruit.quantity -= 1;
+      this.eventFeed.push(new Event("you eat some fruit"));
+    } 
+    else if (this.inventory.meat){
+      this.inventory.food.meat.quantity -= 1;
+      this.eventFeed.push(new Event("you eat some meat"));
+    } 
+    else {
+      this.eventFeed.push(new Event("You have no food to eat"));
+    }
+  });
+
+  let forgeSword = new Action(function(){
+    this.inventory.weapons.sword.quantity += 1;
+    this.eventFeed.push(new Event("You forge a metal stick, Waaay"));
+  });
+  
+  let areas = {
+    hQ: new Area("HeadQuarters", forgeSword, eatFood),
+    forest: new Area("Forest", gatherFruit, hunt)
+  };
+
+  return {
+    areas: areas,
+    focusArea: areas.hQ,
+    eventFeed: events,
+    inventory: inventoryItems
+  }
 }
