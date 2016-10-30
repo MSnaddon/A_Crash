3c94b0f1ac7f0c0326befb50c24332ff5138c424
Michael Snaddon <msnaddon0991@gmail.com>
2016-10-30 17:52:40 +0000

    inventory component finished

--- client/src/components/Inventory.jsx
+++ client/src/components/Inventory.jsx
@@ -2,10 +2,24 @@ const React = require('react');
 
 class Inventory extends React.Component {
   render(){
+    let catagories = []
+
+    for (let catagory in this.props.inventory){
+      const catagoryObject = this.props.inventory[catagory]
+      console.log(catagoryObject);
+      let items = []
+      for (let item in catagoryObject){
+        let itemObject = catagoryObject[item]
+
+        items.push(<div key={itemObject.name}>{itemObject.name} : {itemObject.quantity}</div>)
+      }
+
+      catagories.push(<div key={catagory} className="inventory-catagory">{items}</div>)
+    }
 
     return (
       <div id="inventory">
-        <h3>I am Inventory</h3>
+        {catagories}
       </div>
       )
   }
