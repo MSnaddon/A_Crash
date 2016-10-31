19ef500e860c33d5e4459658d83a412b626bb85f
Michael Snaddon <msnaddon0991@gmail.com>
2016-10-31 18:21:10 +0000

    progression passing all tests

--- client/src/models/Progression.js
+++ client/src/models/Progression.js
@@ -1,14 +1,15 @@
 class Progression{
-  constructor(description, thresholdObject, advanceAction){
+  constructor(description, advanceAction, thresholdObject){
     this.description = description
     this.thresholdObject = thresholdObject
     this.advanceAction = advanceAction
   }
   //alternative in checker is to pass the game, breaks Solid... not sure what this way breaks but it breaks something. Invoking a method that passes itself has got to break some rules. potential stack overflow?
   isPastThreshold(progression){
+    //DO NOT CALL 'progression.isPastThreshold'. NO REASON WHY, SO DON'T DO IT
     //this = the game
     for(let check in progression.thresholdObject){
-      if (!progression.thresholdObject[check].bind(this)()) {return false};
+      if ( !progression.thresholdObject[check].bind(this)() ) return false;
     }
     return true
   }
